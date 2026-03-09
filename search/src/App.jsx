23bda586bc34a132052c078350ad040abeede698
jsbd29 <30559771+jsbd29@users.noreply.github.com>
2026-03-09 12:43:22 -0500

    adding functionality for search

--- search/src/App.jsx
+++ search/src/App.jsx
@@ -4,6 +4,15 @@ import { useState } from "react";
 
 import "./App.css";
 
+
+const list = [
+  "Post 1",
+  "Post 2",
+  "Post 3",
+  "Post 4",,
+  
+]
+
 function App() {
   const [searchValue, setSearchValue] = useState("");
 
@@ -17,12 +26,24 @@ function App() {
 
   const shouldDisplayButton = searchValue.length > 0;
 
+  const filterList = list.filter((list)=>{
+    return list.includes(searchValue)
+  })
+
   return (
     <>
       <div className='card'>
         <div className='container'>
           <input type='text' placeholder = 'Type Something Here... ' value={searchValue} onChange={handleInputChange} />
           { shouldDisplayButton && <button onClick={handleClearClick}> clear </button>}
+          <div>
+            <ul>
+          { filterList.map((list)=>{
+            return <li key={list}>{list}</li>
+          })}
+          </ul>
+          </div>
+
         </div>
       </div>
     </>
